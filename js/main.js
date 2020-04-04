86f3d7c7af60b71be3e11b5bd8e16c8bdcc051f6
Vikulov Andrey <vikulov1@outlook.com>
2020-04-04 13:29:04 +0400

    add final code

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    // nav menu
+    document.querySelector('.menu-icon-wrapper').onclick = function(){
+        document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    };
+    // Owl Corousel
+    $(".owl-carousel").owlCarousel({
+        items:1
+    });
+
+    // Scroll To Top
+
+    const scrollBtn = $('.scroll-to-top-btn');
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 500){
+            scrollBtn.fadeIn();
+        } else {
+            scrollBtn.fadeOut();
+        }
+    });
+    scrollBtn.click(function(){
+        $('html, body').animate({ scrollTop: 0 }, 360);
+        return false;
+    });
+    let productsRow = document.querySelector('.pictore');
+    let mixer = mixitup('.pictore');
+});
